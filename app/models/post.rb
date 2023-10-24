4566d7c9367ed7ed870b76c180b25f9111b393fa
devalibello <alibelloimoukhuede@gmail.com>
2023-10-25 00:46:24 +0300

    Fix linters error in post file

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,6 @@ class Post < ApplicationRecord
   validates :likescounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, presence: true, length: { maximum: 250 }
 
-
-
   def post_counter
     author.update(postcounter: author.posts.count)
   end
