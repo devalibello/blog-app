c5a1ef8f6ff8c288a0f53b10ef1560822c19251c
devalibello <alibelloimoukhuede@gmail.com>
2023-10-30 20:29:03 +0300

    Update router.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources  :users do
-    resources :posts
+  resources  :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 end
