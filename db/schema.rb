ea1361e9940e4fd7af33707519e3b341a8b996f7
devalibello <alibelloimoukhuede@gmail.com>
2023-11-10 20:59:04 +0300

    Add controller for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_04_034327) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_172100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,11 +51,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_04_034327) do
     t.integer "postcounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
