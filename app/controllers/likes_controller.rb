03a32e8fbffc0fe07448654470b51fbfbda4cf5b
devalibello <alibelloimoukhuede@gmail.com>
2023-11-11 17:21:38 +0300

    update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,17 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new
-    @like.author = @current_user
-    @like.post = Post.find(params[:post_id])
-    @like.save!
-    redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+    @like = @current_user.likes.build(post: find_post)
+    
+    if @like.save
+      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]), notice: 'Post liked!'
+    else
+      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]), alert: 'Unable to like the post.'
+    end
+  end
+
+  private
+
+  def find_post
+    Post.find(params[:post_id])
   end
 end
